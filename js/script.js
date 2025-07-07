4781f8a0d565cd7004a68a412cd8988106464eb0
Subhajit Das <subhajitdas12104@gmail.com>
2025-07-07 21:00:21 +0530

    A frontend website is made.

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// js/script.js
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("CareerHunt loaded.");
+});
+
+// You can add theme toggle, animations, or dynamic behaviors here in future
